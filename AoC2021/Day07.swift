a3e116d7e4e31d524ea9cd363979575a140af2b2
Sarah Reichelt <sarah@troz.net>
2021-12-07 15:57:03 +1000

    day 7

--- AoC2021/Day07.swift
+++ AoC2021/Day07.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 func day07(testData: [String], realData: [String]) {
-  let expectedTestResults = [1, 2]
+  let expectedTestResults = [37, 168]
   let testResults = runCode(data: testData)
   if testResults != expectedTestResults {
     print("Error running tests")
@@ -19,7 +19,58 @@ func day07(testData: [String], realData: [String]) {
   print("Results:", realResults)
 
   func runCode(data: [String]) -> [Int] {
+    let crabs = convertToIntegers(data[0].components(separatedBy: ","))
+    let highestNum = crabs.max()!
 
-    return [1, 2]
+    var minFuel = Int.max
+    var bestPosition = -1
+
+    for pos in 0 ... highestNum {
+      var fuel = 0
+      for crab in crabs {
+        fuel += abs(crab - pos)
+      }
+
+      if fuel < minFuel {
+        bestPosition = pos
+        minFuel = fuel
+      }
+    }
+
+    print(bestPosition, minFuel)
+    let result1 = minFuel
+
+    minFuel = Int.max
+    bestPosition = -1
+
+    var moveCosts: [Int: Int] = [0:0]
+    for moves in 1 ... highestNum {
+      moveCosts[moves] = sumOfNumbers(to: moves)
+    }
+
+    for pos in 0 ..< crabs.max()! {
+      var fuel = 0
+      for crab in crabs {
+        let moves = abs(crab - pos)
+        fuel += moveCosts[moves]!
+      }
+
+      if fuel < minFuel {
+        bestPosition = pos
+        minFuel = fuel
+      }
+    }
+
+    print(bestPosition, minFuel)
+    let result2 = minFuel
+
+    return [result1, result2]
+  }
+
+  func sumOfNumbers(to num: Int) -> Int {
+    if num == 1 {
+      return num
+    }
+    return num + sumOfNumbers(to: num - 1)
   }
 }
