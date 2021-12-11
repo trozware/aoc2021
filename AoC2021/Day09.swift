7d3ea2c276fcf9fb9219cce6f052f5b44a18d26f
Sarah Reichelt <sarah@troz.net>
2021-12-11 10:44:33 +1000

    day 9 - part 2

--- AoC2021/Day09.swift
+++ AoC2021/Day09.swift
@@ -46,24 +46,36 @@ func day09(testData: [String], realData: [String]) {
     }
 
     print(result1)
-    print()
 
     // Part 2
 
     var basins: [Int] = []
+
     for coord in lowCoords {
-      let basin = findBasin(data: data, coords: coord)
-      basins.append(basin)
+      var unchecked = [coord]
+      var checked: Set<String> = []
+
+      while !unchecked.isEmpty {
+        let (row, col) = unchecked[0]
+        let id = "\(row),\(col)"
+
+        unchecked = Array(unchecked.dropFirst())
+        if checked.contains(id) {
+          continue
+        }
+        checked.insert(id)
+
+        let adjs = adjacentCoordsNotNine(data: data, rowNum: row, colNum: col)
+        unchecked += adjs
+      }
+      basins.append(checked.count)
     }
 
     let bigBasins = basins.sorted().suffix(3)
-    print(bigBasins)
-
     let result2 = bigBasins.reduce(1) { $0 * $1 }
+
     print(result2)
     print()
-
-    // WRONG: 884058 is too low
 
     return [result1, result2]
   }
@@ -97,111 +109,40 @@ func day09(testData: [String], realData: [String]) {
     return adjs
   }
 
-  func findBasin(data: [String], coords: (Int, Int)) -> Int {
-    let (row, col) = coords
+  func adjacentCoordsNotNine(data: [String], rowNum: Int, colNum: Int) -> [(Int, Int)] {
+    var adjs: [(Int, Int)] = []
 
-    let (centerStr, start, end) = expanduntilNines(fullRow: data[row], startCol: col, endCol: col)
+    let rowBefore = rowNum == 0 ? nil : data[rowNum - 1]
+    let row = data[rowNum]
+    let rowAfter = rowNum == data.count - 1 ? nil : data[rowNum + 1]
 
-    var basinCount = centerStr.count
-    var startCol = start
-    var endCol = end
-    let origStartCol = startCol
-    let origEndCol = endCol
-
-    var rowNum = row - 1
-
-    while rowNum >= 0 {
-      let (newCount, newStart, newEnd) = processNextLine(rowData: data[rowNum], rowNum: rowNum, col: col, start: startCol, end: endCol)
-      if newCount == 0 { break }
-
-      basinCount += newCount
-      startCol = newStart
-      endCol = newEnd
-
-      rowNum -= 1
+    if let rowBefore = rowBefore {
+      let height = rowBefore[colNum]
+      if height != "9" {
+        adjs.append((rowNum - 1, colNum))
+      }
     }
-
-    rowNum = row + 1
-    startCol = origStartCol
-    endCol = origEndCol
-
-    while rowNum < data.count {
-      let (newCount, newStart, newEnd) = processNextLine(rowData: data[rowNum], rowNum: rowNum, col: col, start: startCol, end: endCol)
-      if newCount == 0 { break }
-
-      basinCount += newCount
-      startCol = newStart
-      endCol = newEnd
-
-      rowNum += 1
-    }
-
-    return basinCount
-  }
-
-  func processNextLine(rowData: String, rowNum: Int, col: Int, start: Int, end: Int) -> (Int, Int, Int) {
-    var startCol = start
-    var endCol = end
-
-    var rowEntries = substring(of: rowData, from: startCol, upto: endCol + 1)
-
-    while rowEntries.hasPrefix("9") {
-      rowEntries = String(rowEntries.suffix(rowEntries.count - 1))
-      startCol += 1
-    }
-    while rowEntries.hasSuffix("9") {
-      rowEntries = String(rowEntries.prefix(rowEntries.count - 1))
-      endCol -= 1
-    }
-    if rowEntries.isEmpty { return (0, 0, 0) }
-
-    // now expand to 9 or edge
-    let (centerStr, start, end) = expanduntilNines(fullRow: rowData, startCol: startCol, endCol: endCol)
-    rowEntries = centerStr
-    startCol = start
-    endCol = end
-
-    let noNines = rowEntries.replacingOccurrences(of: "9", with: "")
-    let basinCount = noNines.count
-
-    while rowEntries.contains("9") {
-      // move left or right until the 9 is outside startCol - endCol
-      // *** THIS IS PROBABLY WHERE THE ERROR IS ***
-      if startCol < col {
-        rowEntries = String(rowEntries.suffix(rowEntries.count - 1))
-        startCol += 1
-      } else {
-        rowEntries = String(rowEntries.prefix(rowEntries.count - 1))
-        endCol -= 1
+    if let rowAfter = rowAfter {
+      let height = rowAfter[colNum]
+      if height != "9" {
+        adjs.append((rowNum + 1, colNum))
       }
     }
 
-    return (basinCount, startCol, endCol)
-  }
-
-  func expanduntilNines(fullRow: String, startCol: Int, endCol: Int) -> (String, Int, Int) {
-    var result = substring(of: fullRow, from: startCol, upto: endCol + 1)
-
-    var colNum = startCol - 1
-    while colNum >= 0 {
-      if fullRow[colNum] == "9" {
-        break
+    if colNum > 0 {
+      let height = row[colNum - 1]
+      if height != "9" {
+        adjs.append((rowNum, colNum - 1))
       }
-      result = fullRow[colNum] + result
-      colNum -= 1
     }
-    let start = colNum + 1
 
-    colNum = endCol + 1
-    while colNum < fullRow.count {
-      if fullRow[colNum] == "9" {
-        break
+    if colNum < row.count - 1 {
+      let height = row[colNum + 1]
+      if height != "9" {
+        adjs.append((rowNum, colNum + 1))
       }
-      result += fullRow[colNum]
-      colNum += 1
     }
-    let end = colNum - 1
 
-    return (result, start, end)
+    return adjs
   }
 }
--- AoC2021/main.swift
+++ AoC2021/main.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-let dayNum = 10
+let dayNum = 9
 
 print("Running program for day \(dayNum):")
 print()
